dfa556a6d419ac74d35042dc1772a158334d1def
Rodrigo Rodrigues <rodrigorodriguescosta@gmail.com>
2023-11-13 21:14:46 -0300

    SIG-1151 fixed test

--- comps/go/types/money_test.go
+++ comps/go/types/money_test.go
@@ -1,13 +1,12 @@
-package tests
+package types
 
 import (
-	"comps/types"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestMoney(t *testing.T) {
-	money1 := types.Money(0.91)
+	money1 := Money(0.91)
 	assert.Equal(t, 0.91, money1.GetFloat64())
 }
