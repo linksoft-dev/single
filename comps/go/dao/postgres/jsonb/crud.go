3e5104f22a3d5a0dc3df5b973cbc6d2d0c260eba
Rodrigo Rodrigues <rodrigorodriguescosta@gmail.com>
2024-01-24 01:24:35 -0300

    SIG-1154 refactored jsonb

--- comps/go/dao/postgres/jsonb/crud.go
+++ comps/go/dao/postgres/jsonb/crud.go
@@ -9,6 +9,8 @@ import (
 	"github.com/huandu/go-sqlbuilder"
 	_ "github.com/lib/pq"
 	"github.com/linksoft-dev/single/comps/go/dao"
+	"github.com/linksoft-dev/single/comps/go/db"
+	"github.com/linksoft-dev/single/comps/go/filter"
 	"github.com/linksoft-dev/single/comps/go/obj"
 	log "github.com/sirupsen/logrus"
 	"go.opentelemetry.io/otel"
@@ -21,7 +23,7 @@ import (
 )
 
 const (
-	duplicatedFieldCode = "23505"
+	duplicatedFieldNameCode = "23505"
 )
 
 var tracer trace.Tracer
@@ -31,14 +33,10 @@ func init() {
 }
 
 // NewDataBase factory method para criar uma isntancia da struct Database
-func NewDataBase[T dao.ObjI[T]](ctx context.Context, dbName, tenantId, resourceName string) (*Database[T], error) {
-	db := &Database[T]{
-		ctx:          ctx,
-		db:           dbs[dbName],
-		TenantId:     tenantId,
-		resourceName: resourceName,
+func NewDataBase[T dao.ObjI[T]](dbConnection *gorm.DB) *Database[T] {
+	return &Database[T]{
+		db: dbConnection,
 	}
-	return db, nil
 }
 
 type Doc struct {
@@ -49,17 +47,26 @@ type Doc struct {
 }
 
 type Database[T dao.ObjI[T]] struct {
-	ctx          context.Context
-	TenantId     string
-	resourceName string
-	updateField  dao.UpdateField
-	//db           *sqlx.DB
-	db *gorm.DB
-	tx *gorm.DB // guardar conexao quando for uma transacao
+	updateFieldName dao.UpdateField
+	db              *gorm.DB
+	tx              *gorm.DB // guardar conexao quando for uma transacao
 }
 
-func (d *Database[T]) Create(obj T) (T, error) {
-	list, err := d.Save(true, obj)
+type crudData struct {
+	tableName      string
+	collectionName string
+}
+
+// getTenantInfoFromContext this function returns the crud information needed to perform crud
+// operations
+func getTenantInfoFromContext(ctx context.Context) (r crudData) {
+	r.tableName, _ = ctx.Value("tenantId").(string)
+	r.collectionName, _ = ctx.Value("collectionName").(string)
+	return
+}
+
+func (d *Database[T]) Create(ctx context.Context, obj T) (T, error) {
+	list, err := d.Save(ctx, true, obj)
 	if err != nil {
 		return obj, err
 	}
@@ -70,9 +77,9 @@ func (d *Database[T]) Create(obj T) (T, error) {
 	return obj, nil
 }
 
-func (d *Database[T]) Update(obj T, fields dao.UpdateField) error {
-	d.updateField = fields
-	_, err := d.Save(false, obj)
+func (d *Database[T]) Update(ctx context.Context, obj T, FieldNames dao.UpdateField) error {
+	d.updateFieldName = FieldNames
+	_, err := d.Save(ctx, false, obj)
 	if err != nil {
 		return err
 	}
@@ -80,7 +87,7 @@ func (d *Database[T]) Update(obj T, fields dao.UpdateField) error {
 }
 
 // Save objects whatever is insert or update, based on id the save method decide which operation is
-func (d *Database[T]) Save(insert bool, objs ...T) (list []T, err error) {
+func (d *Database[T]) Save(ctx context.Context, insert bool, objs ...T) (list []T, err error) {
 	if err = d.StartTransaction(); err != nil {
 		return
 	}
@@ -91,6 +98,7 @@ func (d *Database[T]) Save(insert bool, objs ...T) (list []T, err error) {
 	if length < step {
 		step = length
 	}
+	r := getTenantInfoFromContext(ctx)
 	for idx, record := range objs {
 		count++
 		doc, err2 := json.Marshal(record)
@@ -98,7 +106,7 @@ func (d *Database[T]) Save(insert bool, objs ...T) (list []T, err error) {
 			return list, err2
 		}
 		if record.GetId() == "" {
-			err = fmt.Errorf("field id cannot be blank")
+			err = fmt.Errorf("FieldName id cannot be blank")
 			return nil, err
 		}
 
@@ -106,17 +114,18 @@ func (d *Database[T]) Save(insert bool, objs ...T) (list []T, err error) {
 		docStr = strings.ReplaceAll(docStr, "'", "''")
 		if insert {
 			sb.WriteString(fmt.Sprintf(`INSERT INTO %s(id,collection,doc) VALUES ('%s','%s','%s');`,
-				d.TenantId,
+				r.tableName,
 				record.GetId(),
-				d.resourceName,
+				r.collectionName,
 				docStr),
 			)
 		} else {
 			sb.WriteString(fmt.Sprintf(`UPDATE %s SET doc='%s' WHERE id='%s' AND collection = '%s';`,
-				d.TenantId,
+				r.tableName,
 				docStr,
 				record.GetId(),
-				d.resourceName),
+				r.collectionName,
+			),
 			)
 		}
 
@@ -124,12 +133,16 @@ func (d *Database[T]) Save(insert bool, objs ...T) (list []T, err error) {
 		if count == step || idx == length-1 {
 			count = 0
 			result := d.db.Exec(sb.String())
-			if d.createTableIfDoesntExists(result.Error) {
-				result = d.db.Exec(sb.String())
-				if result != nil && result.Error != nil {
-					return nil, result.Error
-				}
+			err := createTableIfDoesntExists(result.Error, d.db, r.tableName, getDocTableDDL(r.tableName))
+			if err != nil {
+				return list, err
 			}
+
+			result = d.db.Exec(sb.String())
+			if result != nil && result.Error != nil {
+				return list, result.Error
+			}
+
 			sb.Reset()
 		}
 	}
@@ -138,41 +151,43 @@ func (d *Database[T]) Save(insert bool, objs ...T) (list []T, err error) {
 	return
 }
 
-func (d *Database[T]) Delete(obj T) error {
-	query := fmt.Sprintf("UPDATE %s set deleted_at=? where id=? and collection=?", d.TenantId)
-	result := d.db.Exec(query, time.Now(), obj.GetId(), d.resourceName)
+func (d *Database[T]) Delete(ctx context.Context, obj T) error {
+	r := getTenantInfoFromContext(ctx)
+	query := fmt.Sprintf("UPDATE %s set deleted_at=? where id=? and collection=?", r.tableName)
+	result := d.db.Exec(query, time.Now(), obj.GetId(), r.collectionName)
 	if result.Error != nil {
 		return result.Error
 	}
 	return nil
 }
 
-func (d *Database[T]) DeleteHard(obj T) error {
-	query := fmt.Sprintf("DELETE FROM %s where id=? and collection=?", d.TenantId)
-	result := d.db.Exec(query, obj.GetId(), d.resourceName)
+func (d *Database[T]) DeleteHard(ctx context.Context, obj T) error {
+	r := getTenantInfoFromContext(ctx)
+	query := fmt.Sprintf("DELETE FROM %s where id=? and collection=?", r.tableName)
+	result := d.db.Exec(query, obj.GetId(), r.collectionName)
 	if result.Error != nil {
 		return result.Error
 	}
 	return nil
 }
 
-func (d *Database[T]) Find(ctx context.Context, filter dao.Query) (records []T, err error) {
+func (d *Database[T]) List(ctx context.Context, f filter.Filter) (records []T, err error) {
 	ctx, span := tracer.Start(ctx, "dao/postgres/jsonb/Find")
 	defer span.End()
-	d.ctx = ctx
+	r := getTenantInfoFromContext(ctx)
 
 	// create sql statement
 	sqlSb := sqlbuilder.NewSelectBuilder()
 	sqlSb.Select("*")
-	sqlSb.From(d.TenantId)
-	setWhere(sqlSb, filter, d.resourceName)
-	setOrderBy(sqlSb, filter)
-	setLimit(sqlSb, filter)
+	sqlSb.From(r.tableName)
+	setWhere(sqlSb, f, r.collectionName)
+	setOrderBy(sqlSb, f)
+	setLimit(sqlSb, f)
 	sqlStatement, args := sqlSb.Build()
 
 	// get docs from database
 	var docs []Doc
-	err = d.Select(&docs, sqlStatement, args)
+	err = d.Select(ctx, &docs, sqlStatement, args)
 	if err != nil {
 		return nil, err
 	}
@@ -198,11 +213,11 @@ func unmarshalDocs[T any](docs []Doc, records *[]T) error {
 	return json.Unmarshal([]byte(str), records)
 }
 
-func (d *Database[T]) Get(id string) (t T, err error) {
-	filter := dao.Query{}
-	filter.First = 1
-	filter.Eq("id", id)
-	records, err := d.Find(d.ctx, filter)
+func (d *Database[T]) Get(ctx context.Context, id string) (t T, err error) {
+	f := filter.Filter{}
+	f.First = 1
+	f.Eq("id", id)
+	records, err := d.List(ctx, f)
 	if err != nil {
 		return
 	}
@@ -211,8 +226,8 @@ func (d *Database[T]) Get(id string) (t T, err error) {
 	}
 	return
 }
-func (d *Database[T]) Select(dest interface{}, query string, args ...interface{}) (err error) {
-	ctx, span := tracer.Start(d.ctx, "dao/postgres/jsonb/Select")
+func (d *Database[T]) Select(ctx context.Context, dest interface{}, query string, args ...interface{}) (err error) {
+	ctx, span := tracer.Start(ctx, "dao/postgres/jsonb/Select")
 	defer span.End()
 	var result *gorm.DB
 	if d.tx != nil {
@@ -221,15 +236,28 @@ func (d *Database[T]) Select(dest interface{}, query string, args ...interface{}
 		result = d.db.Raw(query, args...).Scan(dest)
 	}
 
+	// if select statement has error, check if it's related to missing table, create if it's missing
 	err = result.Error
-	if d.createTableIfDoesntExists(err) {
-		result = d.db.Raw(query, args...).Scan(dest)
-		err = result.Error
-	}
 	if err != nil {
-		log.WithContext(ctx).WithError(err).Error("failed while execute query")
+		r := getTenantInfoFromContext(ctx)
+		err = createTableIfDoesntExists(err, result, r.tableName, r.collectionName)
+		if err != nil {
+			result = d.db.Raw(query, args...).Scan(dest)
+			err = result.Error
+		}
+		if err != nil {
+			log.WithContext(ctx).WithError(err).Error("failed while execute query")
+		}
 	}
+
 	return
+}
+
+func createTableIfDoesntExists(err error, conn *gorm.DB, tableName, ddlScript string) error {
+	if db.IsMissingTableError(err) {
+		return db.CreateTableIfDoesntExists(conn, tableName, ddlScript)
+	}
+	return nil
 }
 
 func (d *Database[T]) StartTransaction() (err error) {
@@ -265,24 +293,24 @@ func (d *Database[T]) RollbackTransaction() (err error) {
 func (d *Database[T]) getValidationError(err error) error {
 	if pqError, ok := err.(*pgconn.PgError); ok {
 		switch pqError.Code {
-		case duplicatedFieldCode:
+		case duplicatedFieldNameCode:
 			values := strings.Split(pqError.Detail, "=")
-			return ViolationUniqKey{Field: values[0], Value: values[1]}
+			return ViolationUniqKey{FieldName: values[0], Value: values[1]}
 		}
 	}
 	return err
 }
 
-func setWhere(sb *sqlbuilder.SelectBuilder, filter dao.Query, resourceName string) {
-	// se estiver consultado com rawquery, nao processe nada, apenas faça o scan para o `dest`
-	if filter.RawQuery == "" {
+func setWhere(sb *sqlbuilder.SelectBuilder, f filter.Filter, resourceName string) {
+	// se econnect 177.54.145.68:27019stiver consultado com rawquery, nao processe nada, apenas faça o scan para o `dest`
+	if f.RawFilter == "" {
 		if resourceName == "" {
 			//return nil, errors.New("Nome da tabela nao foi passado para a Query")
 		}
 
-		// if not include soft deleted, it means need to add filter to make sure bring records were
+		// if not include soft deleted, it means need to add f to make sure bring records were
 		// not psychically deleted
-		if filter.IncludeSoftDeleted == false {
+		if f.IgnoreSoftDelete == false {
 			sb.Where("deleted_at is null")
 		}
 
@@ -294,63 +322,65 @@ func setWhere(sb *sqlbuilder.SelectBuilder, filter dao.Query, resourceName strin
 		}
 
 		// caso nao tenha passado o campo doc, adicione automaticamente
-		for idx, value := range filter.Conditions {
-			if !strings.Contains(value.Field, "doc") {
-				filter.Conditions[idx].Field = fmt.Sprintf("doc ->> '%s'", value.Field)
+		for idx, value := range f.Conditions {
+			if !strings.Contains(value.FieldName, "doc") {
+				f.Conditions[idx].FieldName = fmt.Sprintf("doc ->> '%s'", value.FieldName)
 			}
-
-			for _, c := range filter.Conditions {
-
+			var value interface{}
+			for _, c := range f.Conditions {
+				value = c.Value
 				// converts some special values to the right format
-				switch val := c.Value.(type) {
+				switch val := value.(type) {
 				case time.Time:
-					if val.Hour() == 0 && c.Operator == dao.OperatorLte {
+					if val.Hour() == 0 && c.Operator == filter.Operator_Lte {
 						val = time.Date(val.Year(), val.Month(), val.Day(), 23, 59, 59, 0, time.Local)
 					}
 					c.Value = val.Format("2006-01-02T15:04:05")
 				case bool:
 					c.Value = fmt.Sprintf("%t", c.Value)
 				case float64:
-					c.Field = fmt.Sprintf("COALESCE((%s)::float, 0) %s", c.Field, c.Operator)
+					c.FieldName = fmt.Sprintf("COALESCE((%s)::float, 0) %s", c.FieldName, c.Operator)
 					c.Value = fmt.Sprintf("%f", c.Value)
 				case int:
-					c.Field = fmt.Sprintf("COALESCE((%s)::integer, 0) %s", c.Field, c.Operator)
+					c.FieldName = fmt.Sprintf("COALESCE((%s)::integer, 0) %s", c.FieldName, c.Operator)
 					c.Value = fmt.Sprintf("%d", c.Value)
 				}
 
 				switch c.Operator {
-				case dao.OperatorEquals:
-					if c.Value == nil {
-						sb.Where(sb.IsNull(c.Field))
-					} else {
-						sb.Where(fmt.Sprintf("%s = ?", c.Field))
+				case filter.Operator_Equals:
+					if c.Not {
+						if c.Value == "" {
+							sb.Where(sb.IsNotNull(c.FieldName))
+						} else {
+							sb.Where(sb.NE(c.FieldName, c.Value))
+						}
+						continue
 					}
-				case dao.OperatorNotEquals:
-					if c.Value == nil {
-						sb.Where(sb.IsNotNull(c.Field))
+
+					if c.Value == "" {
+						sb.Where(sb.IsNull(c.FieldName))
 					} else {
-						sb.Where(sb.NE(c.Field, c.Value))
+						sb.Where(fmt.Sprintf("%s = ?", c.FieldName))
 					}
-				case dao.OperatorStarts:
-					sb.Where(fmt.Sprintf("%s ilike %s%%", c.Field, c.Value))
-				case dao.OperatorContains:
-					sb.Where(fmt.Sprintf("%s ilike %%%s%%", c.Field, c.Value))
-				case dao.OperatorIn:
+				case filter.Operator_Starts:
+					sb.Where(fmt.Sprintf("%s ilike %s%%", c.FieldName, c.Value))
+				case filter.Operator_Contains:
+					sb.Where(fmt.Sprintf("%s ilike %%%s%%", c.FieldName, c.Value))
+				case filter.Operator_In:
 					stringArray := obj.ToStringArray(c.Value)
-					sb.Where(sb.In(c.Field, stringArray))
-					continue
-				case dao.OperatorNotIn:
-					stringArray := obj.ToStringArray(c.Value)
-					sb.Where(sb.NotIn(c.Field, stringArray))
-					continue
-				case dao.OperatorGte:
-					sb.Where(sb.GE(c.Field, c.Value))
-				case dao.OperatorGt:
-					sb.Where(sb.G(c.Field, c.Value))
-				case dao.OperatorLte:
-					sb.Where(sb.LE(c.Field, c.Value))
-				case dao.OperatorLt:
-					sb.Where(sb.L(c.Field, c.Value))
+					if c.Not {
+						sb.Where(sb.NotIn(c.FieldName, stringArray))
+						continue
+					}
+					sb.Where(sb.In(c.FieldName, stringArray))
+				case filter.Operator_Gte:
+					sb.Where(sb.GE(c.FieldName, c.Value))
+				case filter.Operator_Gt:
+					sb.Where(sb.G(c.FieldName, c.Value))
+				case filter.Operator_Lte:
+					sb.Where(sb.LE(c.FieldName, c.Value))
+				case filter.Operator_Lt:
+					sb.Where(sb.L(c.FieldName, c.Value))
 				}
 			}
 
@@ -358,50 +388,53 @@ func setWhere(sb *sqlbuilder.SelectBuilder, filter dao.Query, resourceName strin
 	}
 }
 
-func setOrderBy(sb *sqlbuilder.SelectBuilder, filter dao.Query) {
-	//Se o Sort n ta com o cast pro doc, o mesmo deve ser adicionado
-	for i, v := range filter.Sort {
-		if !strings.Contains(i, "doc") {
-			delete(filter.Sort, i)
-			filter.Sort[fmt.Sprintf("doc -> '%s'", i)] = v
+func setOrderBy(sb *sqlbuilder.SelectBuilder, f filter.Filter) {
+
+	//if the order by has no cast to doc field, it means it has to be added
+	for _, v := range f.OrderBy {
+		if v == nil {
+			continue
+		}
+		if !strings.Contains(v.FieldName, "doc") {
+			v.FieldName = fmt.Sprintf("doc -> '%s'", v.FieldName)
 		}
 	}
 
 	// sorting
-	if filter.First > 0 || filter.Last > 0 {
+	if f.First > 0 || f.Last > 0 {
 		sb.OrderBy("doc->>'createdAt' desc").Limit(1)
 	} else {
-		for key, value := range filter.Sort {
-			if value == "asc" {
-				sb.OrderBy(key).Asc()
+		for _, order := range f.OrderBy {
+			if order.Direction == filter.Direction_ASC {
+				sb.OrderBy(order.FieldName).Asc()
 				continue
 			}
-			sb.OrderBy(key).Desc()
+			sb.OrderBy(order.FieldName).Desc()
 		}
 	}
-	// caso seja uma pesquisa padrao, ou seja, passou da condicao acima, então adicione a ordenacao pelo ultimo inserido
-	// caso nao tenha nenhuma instrucao de sort
-	if len(filter.Sort) == 0 {
-		filter.OrderByDesc("doc -> 'createdAt'")
+	// if it is a standard search, that is, it passed the above condition, then add the ordering by last entered
+	// if there is no sort instruction
+	if len(f.OrderBy) == 0 {
+		f.OrderByDesc("doc -> 'createdAt'")
 	}
 }
 
-func setLimit(sb *sqlbuilder.SelectBuilder, filter dao.Query) {
-	if filter.Limit > 0 {
-		sb.Limit(filter.Limit)
+func setLimit(sb *sqlbuilder.SelectBuilder, f filter.Filter) {
+	if f.Limit > 0 {
+		sb.Limit(int(f.Limit))
 	}
 
-	if filter.Page > 1 {
-		sb.Offset((filter.Page - 1) * filter.Limit)
-	}
+	//if f.Page > 1 {
+	//	sb.Offset((f.Page - 1) * f.Limit)
+	//}
 }
 
 type ViolationUniqKey struct {
-	msg   string
-	Field string
-	Value string
+	msg       string
+	FieldName string
+	Value     string
 }
 
 func (v ViolationUniqKey) Error() string {
-	return fmt.Sprintf("Já existe um registro com o valor %s para o campo %s ", v.Value, v.Field)
+	return fmt.Sprintf("Já existe um registro com o valor %s para o campo %s ", v.Value, v.FieldName)
 }
--- comps/go/dao/postgres/jsonb/ddl.go
+++ comps/go/dao/postgres/jsonb/ddl.go
@@ -3,55 +3,7 @@ package jsonb
 import (
 	"fmt"
 	"github.com/huandu/go-sqlbuilder"
-	log "github.com/sirupsen/logrus"
-	"strings"
 )
-
-// createDatabase given all necessary parameters, return error if any
-func createDatabase(host, port, user, password, dbName string, ssl bool) error {
-	db, err := getDbConnection(host, port, user, password, "postgres", ssl)
-	if err != nil {
-		return err
-	}
-	tx := db.Exec(fmt.Sprintf("CREATE DATABASE \"%s\";", dbName))
-	if tx != nil {
-		if tx.Error == nil {
-			return nil
-		}
-		return tx.Error
-	}
-	return fmt.Errorf("failed when try to create database %s, no retunred error message", dbName)
-}
-
-// createTableIfDoesntExists based on the error, check if it's needed to create a table for current tenantId
-func (d *Database[T]) createTableIfDoesntExists(err error) bool {
-	if isMissingTable(err) {
-		log.Warnf("creating table... '%v'", d.TenantId)
-		result := d.db.Exec(getDocTableDDL(d.TenantId))
-		if result != nil {
-			if result.Error == nil {
-				return true
-			}
-			log.WithError(result.Error).Errorf("error while creating table '%s'", d.TenantId)
-		}
-	}
-	return false
-}
-
-// isMissingTable function to return if the error is related to missing table
-func isMissingTable(err error) bool {
-	if err == nil {
-		return false
-	}
-	return strings.Contains(err.Error(), "relation \"") &&
-		strings.Contains(err.Error(), "\" does not exist")
-}
-
-// isMissingDatabase function to return if the error is related of missing database
-func isMissingDatabase(err error, dbName string) bool {
-	msg := fmt.Sprintf("database \"%s\" does not exist", dbName)
-	return strings.Contains(err.Error(), msg)
-}
 
 // getDocTableDDL return the DDL script for DocTable
 func getDocTableDDL(tableName string) string {
