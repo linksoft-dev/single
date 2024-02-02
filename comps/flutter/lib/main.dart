a1e0e2af706c04cbe159fb62e774230212b3d333
Rodrigo Rodrigues <rodrigorodriguescosta@gmail.com>
2024-02-01 23:57:36 -0300

    SIG-1154 changed pages to app

--- comps/flutter/lib/main.dart
+++ comps/flutter/lib/main.dart
@@ -1,60 +1,60 @@
-import 'package:flutter/material.dart';
-import 'package:single/apps/contas/Form.dart';
-import 'package:single/apps/calculator//Form.dart';
-import 'package:single/pages/app.dart';
-
-import 'apps/person/Form.dart';
-
-void main() {
-  List<Pages> pages = [];
-  pages.add(Pages("Pessoa", "/pessoa", (context) => PersonForm()));
-  pages.add(Pages("Contas Receber", "/teste2", (context) => ContasForm()));
-  pages.add(Pages("FatorR", "/fatorr", (context) => CalculaFatorR()));
-  var app = App("crud", pages);
-
-  runApp(app);
-}
-
-
-class FirstScreen extends StatelessWidget {
-  const FirstScreen({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('First Screen'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            // Navigate to the second screen when tapped.
-          },
-          child: const Text('Launch screen'),
-        ),
-      ),
-    );
-  }
-}
-
-class SecondScreen extends StatelessWidget {
-  const SecondScreen({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Second Screen'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            // Navigate back to first screen when tapped.
-          },
-          child: const Text('Go back!'),
-        ),
-      ),
-    );
-  }
-}
-
+// import 'package:flutter/material.dart';
+// import 'package:single/apps/contas/Form.dart';
+// import 'package:single/apps/calculator//Form.dart';
+// import 'package:single/pages/app.dart';
+//
+// import 'apps/person/Form.dart';
+//
+// void main() {
+//   List<Pages> pages = [];
+//   pages.add(Pages("Pessoa", "/pessoa", (context) => PersonForm()));
+//   pages.add(Pages("Contas Receber", "/teste2", (context) => ContasForm()));
+//   pages.add(Pages("FatorR", "/fatorr", (context) => CalculaFatorR()));
+//   var app = App("crud", pages);
+//
+//   runApp(app);
+// }
+//
+//
+// class FirstScreen extends StatelessWidget {
+//   const FirstScreen({Key? key}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('First Screen'),
+//       ),
+//       body: Center(
+//         child: ElevatedButton(
+//           onPressed: () {
+//             // Navigate to the second screen when tapped.
+//           },
+//           child: const Text('Launch screen'),
+//         ),
+//       ),
+//     );
+//   }
+// }
+//
+// class SecondScreen extends StatelessWidget {
+//   const SecondScreen({Key? key}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Second Screen'),
+//       ),
+//       body: Center(
+//         child: ElevatedButton(
+//           onPressed: () {
+//             // Navigate back to first screen when tapped.
+//           },
+//           child: const Text('Go back!'),
+//         ),
+//       ),
+//     );
+//   }
+// }
+//
